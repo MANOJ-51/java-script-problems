c4af4b63966d38bbcec0708e13b6a6837583b825
MANOJ-51 <m4anoj51@gmail.com>
2022-07-29 23:15:15 +0530

    created: program to find the sum of two die values

---
+++ sequence-practice-problems/AddingTwoDieValues.js
@@ -0,0 +1,6 @@
+let dieOne = Math.floor(Math.random() * 6 + 1);
+console.log(dieOne)
+let dieTwo = Math.floor(Math.random() * 6 + 1);
+console.log(dieTwo);
+let sum = dieOne + dieTwo;
+console.log(sum);
