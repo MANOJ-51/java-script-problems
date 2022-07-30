41e6655720819571a9b3b3768b670c945202dcd1
MANOJ-51 <m4anoj51@gmail.com>
2022-07-30 11:53:17 +0530

    created: program to find number and units

---
+++ sequence-practice-problems-if-else if/NumberToUnitConversion.js
@@ -0,0 +1,13 @@
+let numericNumber = 100;
+
+if (numericNumber == 1) {
+    console.log("ones");
+} else if (numericNumber == 10) {
+    console.log("tens");
+} else if (numericNumber == 100) {
+    console.log("hundred");
+} else if (numericNumber == 1000) {
+    console.log("thousand");
+} else {
+    console.log("ten thousand");
+}
