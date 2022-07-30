0e46f531eef146fb890ca7408eeda63f95bab3d3
MANOJ-51 <m4anoj51@gmail.com>
2022-07-30 12:07:01 +0530

    created: number to unit switch

---
+++ secuence-practice-problems-case/NumberToUnitConversion.js
@@ -0,0 +1,19 @@
+let unitNumber = 10;
+
+switch (unitNumber) {
+    case 1:
+        console.log("ones");
+        break;
+    case 10:
+        console.log("tens");
+        break;
+    case 100:
+        console.log("hundrad");
+        break;
+    case 1000:
+        console.log("thousand");
+        break;
+    default:
+        console.log("ten thousand");
+        break;
+}
