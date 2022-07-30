90919b474d74c70417fe465d3dab1e181db9af37
MANOJ-51 <m4anoj51@gmail.com>
2022-07-30 12:01:22 +0530

    created: number to day switch

---
+++ secuence-practice-problems-case/NumberToWeekDay.js
@@ -0,0 +1,25 @@
+let caseNumber = 2;
+
+switch (caseNumber) {
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednesday");
+        break;
+    case 5:
+        console.log("thursday");
+        break;
+    case 6:
+        console.log("friday");
+        break;
+    default:
+        console.log("saturday");
+        break;
+}
