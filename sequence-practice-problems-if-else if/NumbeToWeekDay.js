ee81b96ef394040da1bc25880b0088f3b7e409b0
MANOJ-51 <m4anoj51@gmail.com>
2022-07-30 11:41:06 +0530

    created: program to convert the digit in to word

---
+++ sequence-practice-problems-if-else if/NumbeToWeekDay.js
@@ -0,0 +1,13 @@
+let value = 100;
+
+if (value == 1) {
+    console.log("one");
+} else if (value == 10) {
+    console.log("ten")
+} else if (value == 100) {
+    console.log("hundred");
+} else if (value == 1000) {
+    console.log("thousand");
+} else {
+    console.log("ten Thousand");
+}
