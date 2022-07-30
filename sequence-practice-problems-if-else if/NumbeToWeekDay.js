84559168f03a48d6cb1f36a222096db17797f0d3
MANOJ-51 <m4anoj51@gmail.com>
2022-07-30 11:47:37 +0530

    modified: program to view weekdays

--- sequence-practice-problems-if-else if/NumbeToWeekDay.js
+++ sequence-practice-problems-if-else if/NumbeToWeekDay.js
@@ -1,13 +1,19 @@
-let value = 100;
+let value = 1;
 
 if (value == 1) {
-    console.log("one");
-} else if (value == 10) {
-    console.log("ten")
-} else if (value == 100) {
-    console.log("hundred");
-} else if (value == 1000) {
-    console.log("thousand");
+    console.log("sunday");
+} else if (value == 2) {
+    console.log("monday")
+} else if (value == 3) {
+    console.log("tuesday");
+} else if (value == 4) {
+    console.log("wednesday");
+} else if (value == 5) {
+    console.log("thursday");
+} else if (value == 6) {
+    console.log("friday");
+} else if (value == 7) {
+    console.log("saturday");
 } else {
-    console.log("ten Thousand");
+    console.log("number out of bound");
 }
