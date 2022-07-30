d6262b51596a5e53743f494790eea4d03c323c93
MANOJ-51 <m4anoj51@gmail.com>
2022-07-30 11:27:54 +0530

    created: program to convert digit into word

---
+++ sequence-practice-problems-if-else if/DigitToWord.js
@@ -0,0 +1,23 @@
+let digit = Math.floor(Math.random() * 10)
+
+if (digit == 0) {
+    console.log("zero");
+} else if (digit == 1) {
+    console.log("one");
+} else if (digit == 2) {
+    console.log("two");
+} else if (digit == 3) {
+    console.log("three");
+} else if (digit == 4) {
+    console.log("foure");
+} else if (digit == 5) {
+    console.log("five");
+} else if (digit == 6) {
+    console.log("six");
+} else if (digit == 7) {
+    console.log("seven");
+} else if (digit == 8) {
+    console.log("eight");
+} else {
+    console.log("nine");
+}
